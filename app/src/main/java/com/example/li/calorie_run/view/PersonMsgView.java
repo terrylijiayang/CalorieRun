478b3640c296360ca8c7bcdb94f7bb928449a85d
李锦洪 <1755497577@qq.com>
2018-11-29 21:22:50 +0800

    更新代码

--- app/src/main/java/com/example/li/calorie_run/view/PersonMsgView.java
+++ app/src/main/java/com/example/li/calorie_run/view/PersonMsgView.java
@@ -1,14 +1,6 @@
 package com.example.li.calorie_run.view;
 
 public interface PersonMsgView {
-    void showProgress();
-
-    void hideProgress();
-
-    void setUsernameError();
-
-    void setPasswordError();
-
     void navigateToHome(String value);
 
     void showMsg(String msg);
